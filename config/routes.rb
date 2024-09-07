222c0d7015152c956371a82ebe79da3e65d7728d
sk <sk@skMac.local>
2024-09-07 14:53:58 +0900

    deploy6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  get "top/index"
   root "top#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
